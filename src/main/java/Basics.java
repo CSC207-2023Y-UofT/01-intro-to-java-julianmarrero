cca305ebc09be2ccb2f5b6ec584be8740819ac2a
Julian Alexander Marrero <133060888+julianmarrero@users.noreply.github.com>
2023-06-11 22:40:50 -0400

    Update Basics.java

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -100,7 +100,7 @@ public class Basics {
          * Current count: 0
          */
         for (int i = 10; int i >= 0; i--) {
-            System.out.println("Current count: " + i);
+            System.out.println("Current count:" + i);
         }
 
     }
