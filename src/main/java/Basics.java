531b9331709bc6d88033389001125325de47faab
Julian Alexander Marrero <133060888+julianmarrero@users.noreply.github.com>
2023-06-11 22:29:49 -0400

    Update Basics.java

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -43,7 +43,7 @@ public class Basics {
         /* TODO (Task 1): Write a line of code below that prints the string
          *                Hello World!
          */
-        System.out.println("Hello World!")
+        System.out.println("Hello World!");
 
 
         /* 4. In Python, we could declare variables using a simple assignment
@@ -62,7 +62,7 @@ public class Basics {
          * TODO (Task 2): Create a variable named my_variable and assign it the
          *                value 100.
          */
-        int my_variable = 100
+        int my_variable = 100;
 
 
         /* Do not remove the line below: if you did task 2 correctly, then
@@ -100,7 +100,7 @@ public class Basics {
          * Current count: 0
          */
         for (int i = 10; int i >= 0; i--) {
-            System.out.println("Current count: " + i)
+            System.out.println("Current count: " + i);
         }
 
     }
@@ -144,8 +144,12 @@ public class Basics {
          */
         StringBuilder ret = new StringBuilder();
 
-        // Fill in the rest of the body here
-
+        String[] words = to_split.split(" ");
+        for (String word : words) {
+            if (!word.isEmpty()) {
+                ret.append(word.charAt(0));
+            }
+        }
         return ret.toString();
     }
 
@@ -172,6 +176,13 @@ public class Basics {
          *                You can index into arrays as we do in Python
          *                (e.g. arr[i] gives you the item at index i).
          */
+        if (arr.length < 2) {
+            return 0; // Return 0 if there are no items at odd indices
+        }
+
+        for (int i = 1; i < arr.length; i += 2) {
+            current_sum += array[i];
+        }
 
         return current_sum;
     }
